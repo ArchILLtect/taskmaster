b8f8e71f9236c45af5f1c60a168b7bf5aca577ba
ArchILLtect <140122527+ArchILLtect@users.noreply.github.com>
2025-01-03 11:31:37 -0600

    Remove dotenv and update .env variables

--- src/index.js
+++ src/index.js
@@ -5,19 +5,25 @@ import './index.css';
 import { AppProvider } from "./contexts/AppContext";
 import { Auth0Provider } from '@auth0/auth0-react';
 
-require("dotenv").config();
-
-const domain = process.env.REACT_APP_AUTH0_DOMAIN; // Auth0 domain
-const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID; // Auth0 Client ID
+const awsConfig = {
+    accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
+    region: process.env.REACT_APP_AWS_REGION,
+    bucketName: process.env.REACT_APP_S3_BUCKET_NAME,
+    domain: process.env.REACT_APP_AUTH0_DOMAIN, // Auth0 domain
+    clientId: process.env.REACT_APP_AUTH0_CLIENT_ID // Auth0 Client ID
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root')); // Updated syntax
 root.render(
     <React.StrictMode>
         <Auth0Provider
-            domain={domain}
-            clientId={clientId}
+            domain={awsConfig.domain}
+            clientId={awsConfig.clientId}
             authorizationParams={{
-              redirect_uri: window.location.origin,
+                redirect_uri: window.location.origin,
+                audience: "https://dev-cmwqc1e84xjkhk78.us.auth0.com/api/v2/",
+                scope: "read:current_user update:current_user_metadata"
             }}
         >
             <AppProvider>
