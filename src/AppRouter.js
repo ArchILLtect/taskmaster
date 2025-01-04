539cd503829624e4facf7b9a4db9af982a814dce
ArchILLtect <140122527+ArchILLtect@users.noreply.github.com>
2025-01-04 07:00:21 -0600

    Remove obselete routes

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,8 +1,7 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import App from './App';
 import UserSetupPage from './views/UserSetupPage';
 //import { useInitialRoute } from '../src/services/authHelper';
-import ExternalApi from './views/ExternalApi';
 
 
 const AppRouter = () => {
@@ -10,7 +9,6 @@ const AppRouter = () => {
         <Routes>
             <Route path="/" element={<App />} />
             <Route path="/setup" element={<UserSetupPage />} />
-            <Route path="/external-api" element={<ExternalApi />} />
         </Routes>
     );
 };
