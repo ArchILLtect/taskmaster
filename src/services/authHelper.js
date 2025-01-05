60b56b0183b757ddf35c9f288ad7c44cb3dd810f
ArchILLtect <140122527+ArchILLtect@users.noreply.github.com>
2025-01-04 19:13:06 -0600

    Remove obselete helper function

--- src/services/authHelper.js
+++ src/services/authHelper.js
@@ -1,23 +1,5 @@
 import { useEffect, useState, useCallback } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
-import { getUserInfo } from './apiHelpers';
-import { useUser } from '../contexts/UserContext';
-
-export const useLoadUserOnAuth = () => {
-    const { isAuthenticated, user } = useAuth0();
-    const { currentUser, loadUser } = useUser();
-
-    useEffect(() => {
-        const fetchAndLoadUser = async () => {
-            if (isAuthenticated && !currentUser) {
-                const userData = await getUserInfo(user.sub); // Fetch from backend
-                await loadUser(userData); // Create and cache User instance
-            }
-        };
-
-        fetchAndLoadUser();
-    }, [isAuthenticated, user, currentUser, loadUser]);
-};
 
 export const useCheckSession = () => {
     const { getAccessTokenSilently, logout } = useAuth0();
