16553ff2afc72552b291def5fc74fd73ba73d94f
ArchILLtect <140122527+ArchILLtect@users.noreply.github.com>
2025-01-04 19:13:40 -0600

    Add Auth0 redirect landing page to Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import MainView from "./views/MainView"; // New main view component
+import AuthLandingPage from "./views/AuthLandingPage"
 
 const App = () => {
     return (
             <Routes>
+                <Route path="/landing" element={<AuthLandingPage />} />
                 <Route path="/" element={<MainView />} />
             </Routes>
     );
