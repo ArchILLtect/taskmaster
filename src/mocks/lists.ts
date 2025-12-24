2656d5cb849949a4ed02afcefbba74c325947278
ArchILLtect <nickhansonsr@gmail.com>
2025-12-24 06:26:53 -0600

    feat(mocks): add mock data for TaskList and User

---
+++ src/mocks/lists.ts
@@ -0,0 +1,7 @@
+import type { TaskList } from "../types/list";
+
+export const mockLists: TaskList[] = [
+    { id: "inbox", name: "Inbox" },
+    { id: "school", name: "School" },
+    { id: "work", name: "Work" },
+];
---
+++ src/mocks/user.ts
@@ -0,0 +1,8 @@
+import type { User } from "../types";
+
+export const mockUser: User = {
+    id: "local-dev-user",
+    username: "NickHansonSr",
+    email: "nick@example.com",
+    role: "Admin",
+};
