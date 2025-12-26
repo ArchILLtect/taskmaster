c36390a6a34d6dc44e464600be9cdf8475635780
ArchILLtect <nickhansonsr@gmail.com>
2025-12-26 11:24:42 -0600

    feat(tasks): refactor TasksPage layout to use VStack for improved structure

--- src/pages/TasksPage.tsx
+++ src/pages/TasksPage.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Heading, HStack, Text, VStack } from "@chakra-ui/react";
+import { Box, Heading, HStack, Text, VStack } from "@chakra-ui/react";
 import { mockTasks } from "../mocks/tasks";
 import { TaskRow } from "../components/TaskRow";
 
@@ -6,18 +6,18 @@ export function TasksPage() {
   
   return (
     <VStack minH="100%" p={4} bg="white" rounded="md" boxShadow="sm">
-      <Flex w="100%" gap={4}>
+      <VStack w="100%" gap={4} align="start">
         <Heading size="md">Tasks</Heading>
         {/* TODO: add icon here */}
         <Text>View, sort, and manage all your tasks in one place.</Text>
-        <HStack gap={2} align="start" justify="space-between" w="100%" px={2}>
+        <HStack gap={2} align="end" justify="space-between" w="100%" px={2}>
           <Heading size="sm" w="150px">Task Name & Details</Heading>
-          <HStack align="center" gap={1}>
+          <HStack align="end" gap={1}>
             <Heading size="sm" w="100px" textAlign="right">List</Heading>
             <Heading size="sm" w="100px" textAlign="right">Priority & Status</Heading>
           </HStack>
         </HStack>
-      </Flex>
+      </VStack>
       {mockTasks.length === 0 ? (
         <Text>No tasks available.</Text>
       ) : (
