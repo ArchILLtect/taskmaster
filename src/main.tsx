8c4c508f46b4140263ca49de25c0d819d7aee6a2
ArchILLtect <nickhansonsr@gmail.com>
2026-01-21 00:37:53 -0600

    feat: remove Amplify configuration from main entry point

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
+import "./amplifyConfig";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
-import { Amplify } from "aws-amplify";
-import awsExports from "./aws-exports";
 import App from "./App";
-
-Amplify.configure(awsExports);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
