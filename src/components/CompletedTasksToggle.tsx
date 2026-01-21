e6352e76b0930d787aba9816b0c053bdc12b7c62
ArchILLtect <nickhansonsr@gmail.com>
2026-01-21 02:39:30 -0600

    style: format CompletedTasksToggle component for improved readability

--- src/components/CompletedTasksToggle.tsx
+++ src/components/CompletedTasksToggle.tsx
@@ -3,14 +3,14 @@ import { HStack,Text,Switch } from "@chakra-ui/react";
 export const CompletedTasksToggle = ({ showCompletedTasks, setShowCompletedTasks }: { showCompletedTasks: boolean; setShowCompletedTasks: (value: boolean) => void }) => {
 
 return (
-            <HStack gap={2} align="center">
-              <Text>Show Completed Tasks</Text>
-              <Switch.Root checked={showCompletedTasks} onCheckedChange={() => setShowCompletedTasks(!showCompletedTasks)}>
-                <Switch.HiddenInput />
-                <Switch.Control>
-                  <Switch.Thumb />
-                </Switch.Control>
-                <Switch.Label />
-              </Switch.Root>
-            </HStack>
+  <HStack gap={2} align="center">
+    <Text>Show Completed Tasks</Text>
+    <Switch.Root checked={showCompletedTasks} onCheckedChange={() => setShowCompletedTasks(!showCompletedTasks)}>
+      <Switch.HiddenInput />
+      <Switch.Control>
+        <Switch.Thumb />
+      </Switch.Control>
+      <Switch.Label />
+    </Switch.Root>
+  </HStack>
 )}
