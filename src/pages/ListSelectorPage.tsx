8e743a3dda479748624ddd2d901685e78fcc3cf6
ArchILLtect <nickhansonsr@gmail.com>
2026-01-21 00:38:48 -0600

    feat: refactor ListSelectorPage to use custom hook for data fetching and loading states

--- src/pages/ListSelectorPage.tsx
+++ src/pages/ListSelectorPage.tsx
@@ -1,24 +1,29 @@
 import { VStack, Heading, Text } from "@chakra-ui/react";
 import { SidebarItem } from "../components/SidebarItem";
-import { listService } from "../services/listService";
+import { useListSelectorData } from "../pages/useListSelectorData";
 
 export const ListSelectorPage = () => {
-  const lists = listService.getAll();
+  const { lists, loading, err } = useListSelectorData();
 
   return (
     <VStack align="start" gap={2} minH="100%" p={4} bg="white" rounded="md" boxShadow="sm">
       <Heading size="lg">Lists</Heading>
       <Text>Select a list to view its tasks.</Text>
 
-      {lists.length > 0 ? (
-        <VStack align="stretch" gap={1}>
-          {lists.map((l) => (
-            <SidebarItem key={l.id} to={`/lists/${l.id}`} label={l.name} />
-          ))}
-        </VStack>
-      ) : (
-        <Text>No lists available. Create a new list to get started.</Text>
-      )}
+      {loading ? <Text>Loading…</Text> : null}
+      {err ? <Text>Failed to load lists.</Text> : null}
+
+      {!loading && !err ? (
+        lists.length > 0 ? (
+          <VStack align="stretch" gap={1}>
+            {lists.map((l) => (
+              <SidebarItem key={l.id} to={`/lists/${l.id}`} label={l.name} />
+            ))}
+          </VStack>
+        ) : (
+          <Text>No lists available. Create a new list to get started.</Text>
+        )
+      ) : null}
     </VStack>
   );
-}
+};
