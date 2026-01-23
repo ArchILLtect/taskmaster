bcd25f2958bac9ee4e17e0386ca072d42e1d84bb
ArchILLtect <nickhansonsr@gmail.com>
2026-01-22 18:35:39 -0600

    feat: update fireToast function to accept toast type as a parameter

--- src/hooks/useFireToast.ts
+++ src/hooks/useFireToast.ts
@@ -1,10 +1,10 @@
 import { toaster } from "../components/ui/toasterInstance";
 
-export async function fireToast(title: string, description: string) {
+export async function fireToast(type: "info" | "success" | "error" | "warning", title: string, description: string) {
   return toaster.create({
       title: title || "Feature Not Implemented",
       description: description || "This feature is not yet implemented.",
       duration: 3000,
-      type: "info",
+      type: type,
   });
 }
