30758f5e2d6c1ff8dc18cd20dd6d568c3ba5e378
ArchILLtect <nickhansonsr@gmail.com>
2026-01-29 12:12:28 -0600

    refactor(sidebar): simplify Sidebar component structure and remove variant handling

--- src/layout/Sidebar.tsx
+++ src/layout/Sidebar.tsx
@@ -1,26 +1,11 @@
-import { Box, Separator } from "@chakra-ui/react";
+import { Box, Flex, Separator } from "@chakra-ui/react";
 import { SidebarCollapse } from "../components/SidebarCollapse";
 import { SidebarItem } from "../components/SidebarItem";
 import { viewLinks } from "../config/sidebar";
 import { useListsPageData } from "../pages/useListsPageData";
 
-export function Sidebar({ variant = "app" }: { variant?: "app" | "public" }) {
-  return variant === "public" ? <PublicSidebar /> : <AppSidebar />;
-}
+export function Sidebar() {
 
-function PublicSidebar() {
-  return (
-    <Box minW="18vw" borderRightWidth="1px" p={3} bg="white" boxShadow="sm" position={"sticky"} minH="100%">
-      <SidebarItem to="/" label="Home" main />
-      <Separator my={3} />
-      <SidebarItem to="/about" label="About" main />
-      <Separator my={3} />
-      <SidebarItem to="/login" label="Login" main />
-    </Box>
-  );
-}
-
-function AppSidebar() {
   const { visibleFavorites } = useListsPageData();
 
   const favoriteLinks = visibleFavorites
@@ -30,32 +15,27 @@ function AppSidebar() {
     .map((l) => ({ to: `/lists/${l.id}`, label: l.name }));
 
   return (
-    <Box minW="18vw" borderRightWidth="1px" p={3} bg="white" boxShadow="sm" position={"sticky"} minH="100%">
-      <SidebarItem to="/inbox" label="Inbox" main />
-
-      <Separator my={3} />
-
-      <SidebarCollapse to="/views" label="Views" items={viewLinks} defaultOpen={false} />
-
-      <Separator my={3} />
-
-      <SidebarItem to="/tasks" label="Tasks" main />
-
-      <Separator my={3} />
-
-      <SidebarItem to="/updates" label="Updates" main />
-
-      <Separator my={3} />
-
-      <SidebarItem to="/lists" label="Lists" main />
-
-      <Separator my={3} />
-
-      <SidebarCollapse to="/favorites" label="Favorites" items={favoriteLinks} defaultOpen={false} />
-
-      <Separator my={3} />
-
-      <SidebarItem to="/dev" label="Dev" main />
-    </Box>
+    <Flex flexDirection={"column"} justifyContent={"space-between"} minW="18vw" borderRightWidth="1px" p={3} bg="white" boxShadow="sm" position={"sticky"} minH="100%">
+      <Box>
+        <SidebarItem to="/inbox" label="Inbox" main />
+        <Separator my={3} />
+        <SidebarCollapse to="/views" label="Views" items={viewLinks} defaultOpen={false} />
+        <Separator my={3} />
+        <SidebarItem to="/tasks" label="Tasks" main />
+        <Separator my={3} />
+        <SidebarItem to="/updates" label="Updates" main />
+        <Separator my={3} />
+        <SidebarItem to="/lists" label="Lists" main />
+        <Separator my={3} />
+        <SidebarCollapse to="/favorites" label="Favorites" items={favoriteLinks} defaultOpen={false} />
+        <Separator my={3} />
+        <SidebarItem to="/dev" label="Dev" main />
+      </Box>
+      <Box mt={6}>
+        <SidebarItem to="/" label="Home" main />
+        <Separator my={3} />
+        <SidebarItem to="/about" label="About" main />
+      </Box>
+    </Flex>
   );
 }
