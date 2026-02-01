61ca1b7cb6e0b5b17e84901a3ad12453852dcad9
ArchILLtect <nickhansonsr@gmail.com>
2026-02-01 13:50:56 -0600

    feat(AppCollapsible): add new collapsible component for better UI organization

---
+++ src/components/AppCollapsible.tsx
@@ -0,0 +1,45 @@
+
+import { Text, Box, HStack, Collapsible } from "@chakra-ui/react";
+import { FiChevronDown } from "react-icons/fi";
+
+type AppCollapsibleProps = {
+  title: string;
+  children: React.ReactNode;
+  defaultOpen?: boolean;
+  width?: string;
+  mt?: string;
+  mb?: string;
+};
+
+export function AppCollapsible({ title, children, defaultOpen = false, width = "100%", mt = "5", mb = "5" }: AppCollapsibleProps) {
+  return (
+    <Collapsible.Root defaultOpen={defaultOpen} w={width} mt={mt} mb={mb}>
+      <Collapsible.Trigger asChild>
+        <HStack
+          paddingRight={2}
+          cursor="pointer"
+          userSelect="none"
+          justify="space-between"
+          rounded="md"
+          _hover={{ bg: "blackAlpha.50" }}
+        >
+          <Text fontSize="lg" fontWeight="600">{title}</Text>
+
+          {/* rotate chevron when open */}
+          <Collapsible.Indicator asChild>
+            <Box transition="transform 150ms" _open={{ transform: "rotate(180deg)" }}>
+              <FiChevronDown />
+            </Box>
+          </Collapsible.Indicator>
+        </HStack>
+      </Collapsible.Trigger>
+      <Collapsible.Content>
+
+      {/* content goes here */}
+      {children}
+
+      </Collapsible.Content>
+    </Collapsible.Root>
+
+  )
+}
