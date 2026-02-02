17b754e2d0338dbcdc8180e9c5f96570aec8f354
ArchILLtect <nickhansonsr@gmail.com>
2026-02-02 00:58:06 -0600

    feat(Sidebar): enhance tooltip components with arrow display for overdue and due soon tasks

--- src/layout/Sidebar.tsx
+++ src/layout/Sidebar.tsx
@@ -37,12 +37,12 @@ export function Sidebar() {
             hasOverdue || hasDueSoon ? (
               <>
                 {hasOverdue ? (
-                  <Tooltip content="You have overdue tasks">
+                  <Tooltip content="You have overdue tasks" showArrow>
                     <Icon as={FcExpired} boxSize={5} aria-label="Overdue tasks" />
                   </Tooltip>
                 ) : null}
                 {hasDueSoon ? (
-                  <Tooltip content="You have tasks due soon">
+                  <Tooltip content="You have tasks due soon" showArrow>
                     <Icon as={FcHighPriority} boxSize={5} aria-label="Due soon tasks" />
                   </Tooltip>
                 ) : null}
